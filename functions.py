5c46b8a0657212faf054a8f10dcfb97c726e909f
[Sander Belon] <[30241262@fsv.cuni.cz]>
2023-07-26 19:29:25 +0200

    added docstring for functions.flr_duration

--- functions.py
+++ functions.py
@@ -11,24 +11,25 @@ import pandas as pd
 
 def flr_duration(z):
     """
+    Convert start time and end time columns to datetime format and calculate solar flare duration.
 
-    Calculate the duration of solar flares from the 'FLR' DataFrame.
-
-    This function takes the 'FLR' DataFrame, assumed to be part of the 'data_frames' dictionary,
-    and converts the 'beginTime' and 'endTime' columns to datetime format. Then, it calculates
-    the duration of each solar flare and adds a new column 'duration' to the DataFrame.
+    This function takes a DataFrame 'z' containing solar flare information and assumes that the
+    'beginTime' and 'endTime' columns are present in the DataFrame. It converts these columns to
+    datetime format using pandas' pd.to_datetime() function. Then, it calculates the duration of
+    each solar flare and adds a new column 'duration' to the DataFrame.
 
     Parameters:
-    None (The function assumes that 'FLR' DataFrame is available in the 'data_frames' dictionary.)
+    z (pandas.DataFrame): The DataFrame containing solar flare information.
 
     Returns:
-    None (The function modifies the 'FLR' DataFrame in-place, adding the 'duration' column.)
+    None: The function modifies the DataFrame 'z' in-place, adding the 'duration' column.
 
     Example:
-    If 'FLR' DataFrame has columns ['beginTime', 'endTime'], the function will add a new column
+    If 'z' DataFrame has columns ['beginTime', 'endTime'], the function will add a new column
     'duration' containing the duration of each solar flare in minutes.
 
     """
+    
     # Convert the start time and end time columns to datetime format
     z['beginTime'] = pd.to_datetime(z['beginTime'])
     z['endTime'] = pd.to_datetime(z['endTime'])
