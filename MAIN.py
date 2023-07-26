3bc2b7ffc0f479c28750daa1883387ac2465a305
[Sander Belon] <[30241262@fsv.cuni.cz]>
2023-07-26 18:55:02 +0200

    updated graphs for flr_df

--- MAIN.py
+++ MAIN.py
@@ -4,15 +4,16 @@ print(donki_api_data.download_donki_data.__doc__)
 # Call the function to download the data and store it in a dictionary of DataFrames
 data_frames = donki_api_data.download_donki_data()
 
-# Now you can work with the data stored in the data_frames dictionary
+# Now we can work with the data stored in the data_frames dictionary
 print(data_frames.keys())
 
 from functions import solar_flare_df
-import pandas as pd
 
 print(solar_flare_df.__doc__)
 solar_flare_df(data_frames)
 flr_df = data_frames['FLR']
 flr_df['duration'].describe()
 
-# flr_hist(flr_df)
+import graphs
+graphs.flr_hist(flr_df)
+graphs.flr_class(flr_df)
--- graphs.py
+++ graphs.py
@@ -1,6 +1,76 @@
-import pandas as pd
+"""
+This module provides functions for visualizing the space weather data.
 
-def flr_hist(flr_df):
-    # Find the index of the minimum value in the 'duration_minutes' column
-    min_index = flr_df["duration"].idxmin()
-    flr_df.iloc[min_index]
+Functions:
+- flr_hist(x): Create a histogram of solar flare duration without outliers.
+
+"""
+import matplotlib.pyplot as plt
+
+def flr_hist(x):
+    """
+    Create a histogram of solar flare duration without outliers.
+
+    This function takes a DataFrame 'x' containing information about solar flares,
+    and it calculates the range of solar flare duration without outliers using
+    quantiles (1st and 99th percentile). Then, it creates a histogram to visualize
+    the distribution of solar flare durations without including the outliers.
+
+    Parameters:
+    x (pandas.DataFrame): A DataFrame containing solar flare information.
+
+    Returns:
+    None (The function displays the histogram plot).
+
+    Example:
+    flr_hist(data_frame)
+
+    """
+    # Calculate the range of solar flare duration without outliers
+    lower_bound = x['duration'].quantile(0.01)
+    upper_bound = x['duration'].quantile(0.99)
+
+    # Create a histogram of the solar flare duration without outliers
+    plt.hist(x['duration'], bins=20, range=(lower_bound, upper_bound))
+    plt.xlabel('Solar Flare Duration (minutes)')
+    plt.ylabel('Frequency')
+    plt.title('Histogram of Solar Flare Duration (without outliers)')
+    plt.show()
+
+def flr_class(x):
+    """
+    Create a bar chart of solar flare class frequencies.
+
+    This function takes a DataFrame 'x' containing information about solar flares,
+    and it filters the DataFrame to include only rows with 'classType' values that
+    occur more than 2 times. Then, it creates a bar chart to visualize the frequency
+    distribution of solar flare classes with frequencies greater than 2.
+
+    Parameters:
+    x (pandas.DataFrame): A DataFrame containing solar flare information.
+
+    Returns:
+    None (The function displays the bar chart plot).
+
+    Example:
+    flr_class(data_frame)
+
+    Notes:
+    - The 'classType' column in the DataFrame 'x' must contain categorical values
+      representing solar flare classes.
+    - The bar chart displays the frequency distribution of solar flare classes
+      that occur more than 2 times.
+    - The x-axis represents the solar flare classes, and the y-axis represents
+      the frequency count for each class.
+
+    """
+    # Filter the DataFrame to include only rows with 'classType' values that occur more than 2 times
+    class_counts = x['classType'].value_counts()
+    class_counts_filtered = class_counts[class_counts > 2]
+
+    # Create a bar chart of the filtered classType
+    class_counts_filtered.plot(kind='bar', rot=45)
+    plt.xlabel('Solar Flare Class')
+    plt.ylabel('Frequency')
+    plt.title('Bar Chart of Solar Flare Class (Frequency > 2)')
+    plt.show()
