999a6b5fe6941b1e0cf64b27223adeb50693b30e
[Sander Belon] <[30241262@fsv.cuni.cz]>
2023-07-26 13:51:03 +0200

    general changes to all files

--- MAIN.py
+++ MAIN.py
@@ -7,8 +7,12 @@ data_frames = donki_api_data.download_donki_data()
 # Now you can work with the data stored in the data_frames dictionary
 print(data_frames.keys())
 
-import functions
+from functions import solar_flare_df
+import pandas as pd
 
-print(functions.solar_flare_df.__doc__)
-functions.solar_flare_df(data_frames)
-# flr_df['duration'].describe()
+print(solar_flare_df.__doc__)
+solar_flare_df(data_frames)
+flr_df = data_frames['FLR']
+flr_df['duration'].describe()
+
+# flr_hist(flr_df)
--- graphs.py
+++ graphs.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def flr_hist():
+def flr_hist(flr_df):
     # Find the index of the minimum value in the 'duration_minutes' column
     min_index = flr_df["duration"].idxmin()
     flr_df.iloc[min_index]
